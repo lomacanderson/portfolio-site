43c4f7b315e290f840739a1e2de77fa69d8f1bf2
lomacanderson <voiceoverlogan@gmail.com>
2025-05-01 17:55:42 -0700

    added new domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   preview: {
-    allowedHosts: ['process.env.VITE_ALLOWED_HOST']
+    allowedHosts: ['logananderson.dev'],
   },
   plugins: [
   react(),
