99ac9580fd2396a1f47d1c6646ca620713b92686
lomacanderson <voiceoverlogan@gmail.com>
2025-05-01 17:44:17 -0700

    add allowedHosts to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    allowedHosts: ['process.env.VITE_ALLOWED_HOST']
+  },
   plugins: [
   react(),
   tailwindcss(),
