be5825b01559b42806f443abdf05cd74f577ec89
lomacanderson <voiceoverlogan@gmail.com>
2025-05-09 02:21:48 -0700

    Add dynamic sizing for footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-<footer className="p-4 px-25">
+<footer className="p-4 px-10 md:px-15 lg:px-25">
 <p className="text-xl">&copy;{new Date().getFullYear()} Logan Anderson</p>
 </footer>
     )
