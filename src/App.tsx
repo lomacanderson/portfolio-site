a89d6abea9e2039ed1c1b0012f57d331597a15ee
lomacanderson <voiceoverlogan@gmail.com>
2026-08-02 20:46:49 -0700

    Add animated 3D GLSL city shader background component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Footer } from './components/footer.tsx'
 import { Header } from './components/header.tsx'
 import { Main } from './components/main.tsx'
+import { ShaderBackground } from './components/ShaderBackground.tsx'
 
 function App() {
     return (
       <>
+        <ShaderBackground />
         <Header></Header>
         <Main></Main>
         <Footer></Footer>
